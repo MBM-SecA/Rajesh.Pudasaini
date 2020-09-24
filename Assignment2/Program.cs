e4ff3a8111088a1a5f9570a43206be6d31a646bd
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-09-24 09:17:59 +0545

    main file to execute the program

---
+++ Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿//All the assignment1 question
+
+using System;
+class Assignment1 {
+    static void Main(){
+        CreateDirExecute obj1 = new CreateDirExecute();
+        bool res = obj1.CreateSubDir();
+        if (res){
+            Console.WriteLine("Directory and sub-directory created and added program source inside it");
+        }
+    }
+}
