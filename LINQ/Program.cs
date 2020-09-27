ea17209e3bb8482a4d5f2ced5ff3ffef48dd23c1
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-09-27 09:09:22 +0545

    learn quantifier, partitioning, repeating, projection

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,7 +10,7 @@ namespace LINQ
         {
             //LINQ - Language Integrated Query
             //LINQ to objects
-            int[] numbers = {1, 2, 3, 4, 5, 78, 99};
+            int[] numbers = {1, 2, 3, 4, 5, 78, 99, 0, 6, 9};
 
             //fetch numbers which are greater than 50
             var numbersGreaterThan50 = numbers.Where(x => x > 50);
@@ -40,7 +40,55 @@ namespace LINQ
                                     select country;
 
 
+            //Projection
+
+            var result = numbers.Select(x => x * 2);
+            var result2 = from num in numbers
+                            select num * num;
+            foreach(var r in result){
+                Console.WriteLine(r);
+            }
+
+            var result3 = from num in numbers
+                            orderby num
+                            select num;
+
+            foreach(var r in result3){
+                Console.WriteLine(r);
+            }
             
+            var result4 = from name in names
+                            orderby name 
+                            select name;
+
+            foreach(var r in result4){
+                Console.WriteLine(r);
+            }
+
+            var result5 = numbers.OrderBy(x => x).Take(5);
+            foreach(var r in result5){
+                Console.WriteLine(r);
+            }
+            
+            //Partitioning
+            var result6 = numbers.Skip(5).Take(5);
+
+            foreach(var r in result6){
+                Console.WriteLine(r);
+            }
+
+            //Quantifier
+            var result8 = numbers.Any(x => x % 2 ==0 ); //Any returns boolean
+            var result9 = numbers.All(x => x % 2 ==0 ); //Any returns boolean
+            var result10 = numbers.Contains(0);
+
+            //Repeating
+            var result11 = Enumerable.Range(1, 1000);
+            var result12 = Enumerable.Repeat("Hello World", 10);
+
+            foreach(var r in result12){
+                Console.WriteLine(r);
+            }
 
         }
     }
