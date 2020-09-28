91821d9a99ccf2a4e4e09c83b3638c2878f45fdd
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-09-28 20:02:11 +0545

    comment out some of console output

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -23,17 +23,17 @@ namespace LINQ
             //names having length greater than 3 and startswith R
             string[] names = {"bishnu", "rajesh", "ramesh", "shyam", "hari", "a", "Raygon", "Ramsy"};
             var lengthGreaterThanThreeAndStartWithR = names.Where(x => x.Length > 3 && x.ToLower().StartsWith('r'));
-            foreach(string name in lengthGreaterThanThreeAndStartWithR){
-                Console.WriteLine($"Result1: {name}");
-            } 
+            // foreach(string name in lengthGreaterThanThreeAndStartWithR){
+            //     Console.WriteLine($"Result1: {name}");
+            // } 
             var lengthGreaterThanThreeAndStartWithRAnother = from name in names 
                                                             where name.Length > 3
                                                             && name.ToLower().StartsWith('r')
                                                             select name;
 
-            foreach(string name in lengthGreaterThanThreeAndStartWithRAnother){
-                Console.WriteLine($"Result2: {name}");
-            } 
+            // foreach(string name in lengthGreaterThanThreeAndStartWithRAnother){
+            //     Console.WriteLine($"Result2: {name}");
+            // } 
             var countries = Country.GetCountries();
             var asianCountries = from country in countries
                                     where country.Continent == "Asia"
