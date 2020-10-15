eacb134e439156c507ca691815390d1dba192d3e
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-10-15 10:48:52 +0545

    employee detail

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,26 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 
 public class EmployeeController: Controller 
 {
-    public ActionResult Index() {
 
-        List<Person> persons = Person.GetEmployee();
+    public List<Person> persons = Person.GetEmployee();
+
+    //listing employee list
+    public ActionResult Index() {
+        // var persons = Person.GetEmployee();
         return View(persons);
     }
 
-    public ActionResult Detail(string firstName){
-        List<Person> persons = Person.GetEmployee();
-        Person p1 = null;
-        foreach(var p in persons){
-            if (p.FirstName == firstName){
-                p1 = p;
-            }
-        }
-        if (p1 != null){
-            return View(p1);
-        }
+    //listing employee detail
+    //[FromQuery] is attribute
+    // [Route("summary")] if we want to change url
+    public ActionResult Detail(int id){
+        // var persons = Person.GetEmployee();
+        // Person p1 = null;
+        // foreach(var p in persons){
+        //     if (p.FirstName == firstName){
+        //         p1 = p;
+        //     }
+        // }
+        // if (p1 != null){
+        //     return View(p1);
+        // }
+        // return View();
+       
+        var p1 = from person in persons
+                     where person.Id == id
+                     select person;
+
+        return View(p1.FirstOrDefault());
+    }
+
+    //view to add employee
+    public ActionResult Add(){
         return View();
     }
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 public class Person {
-    public Person(string firstName, string lastName, char gender, long contact, string address=default) {
+    public Person(int id, string firstName, string lastName, char gender, long contact, string address=default) {
+        Id = id;
         FirstName = firstName;
         LastName = lastName;
         Address = address;
         Gender = gender;
         Contact = contact;
     }
+
+    public int Id {get; set;}
     public string FirstName {get; set;}
     public string LastName {get; set;}
 
@@ -17,11 +21,11 @@ public class Person {
     public long Contact {get; set; }
 
     public static List<Person> GetEmployee(){
-        Person p1 = new Person("Rajesh", "Pudasaini", 'M', 9860476499, "Bhaktapur");
-        Person p2 = new Person("Rajendra", "Pudasaini", 'M', 9860476499, "Syangja");
-        Person p3 = new Person("Kabita", "Pudasaini", 'F', 9860476499);
-        Person p4 = new Person("Krishna", "Pudasaini", 'F', 9860476499);
-        Person p5 = new Person("Sunil", "KC", 'M', 9860476499);
+        Person p1 = new Person(1, "Rajesh", "Pudasaini", 'M', 9860476499, "Bhaktapur");
+        Person p2 = new Person(2, "Rajendra", "Pudasaini", 'M', 9860476499, "Syangja");
+        Person p3 = new Person(3, "Kabita", "Pudasaini", 'F', 9860476499);
+        Person p4 = new Person(4, "Krishna", "Pudasaini", 'F', 9860476499);
+        Person p5 = new Person(5, "Sunil", "KC", 'M', 9860476499);
 
         List<Person> persons = new List<Person>{p1, p2, p3, p4, p5};
         return persons;
