86ab9be0e7c6ff627b52f52f0a0747366f5a4f95
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-12-01 08:15:15 +0545

    remove static data

--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -31,14 +31,14 @@ public class Person {
     public char Gender { get; set; }
     public long? Contact {get; set; }
 
-    public static List<Person> GetEmployee(){
-        Person p1 = new Person(1, "Rajesh", "Pudasaini", 'M', 9860476499, "Bhaktapur");
-        Person p2 = new Person(2, "Rajendra", "Pudasaini", 'M', 9860476499, "Syangja");
-        Person p3 = new Person(3, "Kabita", "Pudasaini", 'F', 9860476499, "Kapan");
-        Person p4 = new Person(4, "Krishna", "Pudasaini", 'F', 9860476499, "Kathmandu");
-        Person p5 = new Person(5, "Sunil", "KC", 'M', 9860476499, "Baneshwor");
+    // public static List<Person> GetEmployee(){
+    //     Person p1 = new Person(1, "Rajesh", "Pudasaini", 'M', 9860476499, "Bhaktapur");
+    //     Person p2 = new Person(2, "Rajendra", "Pudasaini", 'M', 9860476499, "Syangja");
+    //     Person p3 = new Person(3, "Kabita", "Pudasaini", 'F', 9860476499, "Kapan");
+    //     Person p4 = new Person(4, "Krishna", "Pudasaini", 'F', 9860476499, "Kathmandu");
+    //     Person p5 = new Person(5, "Sunil", "KC", 'M', 9860476499, "Baneshwor");
 
-        List<Person> persons = new List<Person>{p1, p2, p3, p4, p5};
-        return persons;
-    }
+    //     List<Person> persons = new List<Person>{p1, p2, p3, p4, p5};
+    //     return persons;
+    // }
 }
